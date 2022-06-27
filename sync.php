6127ad2dadb1d8de492b1ada92216b049063d8b1
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-06-27 21:44:14 +0700

    Code Review

--- sync.php
+++ sync.php
@@ -1,5 +1,6 @@
 <?php
 echo "<pre>";
+$schema      = 'public';
 $table       = 'daya_tampung';
 $host        = "host = localhost";
 $port        = "port = 5432";
@@ -8,32 +9,30 @@ $credentials = "user = postgres password=root";
 $url         = '192.168.4.28:8082/syncserver/server.php?q=push_data_server';
 
 $count_per_batch = 30;
-
-$conn = pg_connect( "$host $port $dbname $credentials"  );
-if (!$conn) {
-    // echo "Error : Unable to open database\n";
-} else {
-    // echo "Opened database successfully\n";
-}
-
 $primaries = ['id_smt', 'id_sms'];
 
-$sql_columns = " SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '$table';";
+$conn = pg_connect("$host $port $dbname $credentials");
+if (!$conn) {
+    echo "Error : Unable to open database\n";
+    exit;
+}
+
+$sql_columns = "SELECT column_name FROM information_schema.columns WHERE table_schema = '$schema' AND table_name = '$table';";
 $res = pg_query($conn, $sql_columns);
-while($row = pg_fetch_row($res)) {
+while ($row = pg_fetch_row($res)) {
     $structures[] = $row[0];
 }
 
-$sql_data = " SELECT * FROM daya_tampung WHERE last_update > last_sync";
+$sql_data = "SELECT * FROM $table WHERE last_update > last_sync;";
 $res = pg_query($conn, $sql_data);
-while($row = pg_fetch_row($res)) {
+while ($row = pg_fetch_row($res)) {
     $rows[] = $row;
 }
 
 $length = count($rows);
-$batch = ceil($length/$count_per_batch);
+$batch = ceil($length / $count_per_batch);
 $post = [
-    'schema' => 'public',
+    'schema' => $schema,
     'table' => $table,
     'primary_keys' => $primaries,
     'struktur' => $structures
@@ -41,31 +40,32 @@ $post = [
 
 for ($i = 1; $i <= $batch; $i++) {
     $rows_send = [];
-    for ($j = $count_per_batch * ($i-1); $j < $count_per_batch * $i; $j++) {
+    for ($j = $count_per_batch * ($i - 1); $j < $count_per_batch * $i; $j++) {
         if (!empty($rows[$j])) {
             $rows_send[] = $rows[$j];
         }
     }
-    
+
     $post['rows'] = $rows_send;
     $req = base64_encode(json_encode($post));
-    $fields = ['p'=> $req];
+    $fields = ['p' => $req];
     $fields_string = http_build_query($fields);
     $ch = curl_init();
-    
-    curl_setopt($ch,CURLOPT_URL, $url);
-    curl_setopt($ch,CURLOPT_POST, true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $result = curl_exec($ch);
     $encoded_result = json_decode(base64_decode($result));
-    
+
     curl_close($ch);
 
-    if (!empty($encoded_result->error_desc) ) {
+    if (!empty($encoded_result->error_desc)) {
         echo "Error $encoded_result->error_code : $encoded_result->error_desc";
     }
+
     echo "Success : " . $encoded_result->data->success->total . ", Failed : " . $encoded_result->data->failed->total . "<br/>";
     echo "--------------------------------------<br/>";
 }
